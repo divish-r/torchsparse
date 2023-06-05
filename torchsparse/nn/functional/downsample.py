4d820f15ad6028e2163c8cb9ac80bcfbaeb5bf13
divish-r <80689273+divish-r@users.noreply.github.com>
2023-06-05 12:45:08 +0100

    update to rounding_mode="trunc"

--- torchsparse/nn/functional/downsample.py
+++ torchsparse/nn/functional/downsample.py
@@ -26,7 +26,8 @@ def spdownsample(
         coords = coords.clone()
         coords[:, :3] = torch.div(
             coords[:, :3],
-            sample_stride.float()).trunc() * sample_stride  # type: ignore
+            sample_stride.float(),
+            rounding_mode='trunc') * sample_stride  # type: ignore
         coords = coords[:, [3, 0, 1, 2]]
         coords = torch.unique(coords, dim=0)
         coords = coords[:, [1, 2, 3, 0]]
